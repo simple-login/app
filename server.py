5e2fc603cd36b112fc64f9e405f3b605c11bf646
Son NK <son@imac-de-son.home>
2019-07-06 17:04:26 +0200

    set DEBUG_TB_INTERCEPT_REDIRECTS=True in local

--- server.py
+++ server.py
@@ -265,6 +265,7 @@ if __name__ == "__main__":
     app.debug = True
 
     app.config["DEBUG_TB_PROFILER_ENABLED"] = True
+    app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
     toolbar = DebugToolbarExtension(app)
 
