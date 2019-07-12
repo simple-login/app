70e4f8b6e64966b07e5cbeedff1d42a9fe5b01a7
Son NK <son@imac-de-son.home>
2019-07-12 11:34:29 +0200

    use os.environ[] instead of os.environ.get to fail fast

--- app/config.py
+++ app/config.py
@@ -24,40 +24,40 @@ else:
     load_dotenv()
 
 
-URL = os.environ.get("URL") or "http://sl-server:7777"
+URL = os.environ["URL"]
 print("URL:", URL)
 
-EMAIL_DOMAIN = os.environ.get("EMAIL_DOMAIN") or "sl"
-SUPPORT_EMAIL = os.environ.get("SUPPORT_EMAIL") or "support@sl"
-SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
-DB_URI = os.environ.get("DB_URI") or "sqlite:///db.sqlite"
+EMAIL_DOMAIN = os.environ["EMAIL_DOMAIN"]
+SUPPORT_EMAIL = os.environ["SUPPORT_EMAIL"]
+SENDGRID_API_KEY = os.environ["SENDGRID_API_KEY"]
+DB_URI = os.environ["DB_URI"]
 
-FLASK_SECRET = os.environ.get("FLASK_SECRET") or "secret"
+FLASK_SECRET = os.environ["FLASK_SECRET"]
 
 # invalidate the session at each new version by changing the secret
 FLASK_SECRET = FLASK_SECRET + SHA1
 
 ENABLE_SENTRY = "ENABLE_SENTRY" in os.environ
-ENV = os.environ.get("ENV")
+ENV = os.environ["ENV"]
 
 AWS_REGION = "eu-west-3"
-BUCKET = os.environ.get("BUCKET") or "local.sl"
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+BUCKET = os.environ["BUCKET"]
+AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
 
 ENABLE_CLOUDWATCH = "ENABLE_CLOUDWATCH" in os.environ
-CLOUDWATCH_LOG_GROUP = os.environ.get("CLOUDWATCH_LOG_GROUP")
-CLOUDWATCH_LOG_STREAM = os.environ.get("CLOUDWATCH_LOG_STREAM")
+CLOUDWATCH_LOG_GROUP = os.environ["CLOUDWATCH_LOG_GROUP"]
+CLOUDWATCH_LOG_STREAM = os.environ["CLOUDWATCH_LOG_STREAM"]
 
-STRIPE_API = os.environ.get("STRIPE_API")  # Stripe public key
-STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
-STRIPE_YEARLY_PLAN = os.environ.get("STRIPE_YEARLY_PLAN")
-STRIPE_MONTHLY_PLAN = os.environ.get("STRIPE_MONTHLY_PLAN")
+STRIPE_API = os.environ["STRIPE_API"]  # Stripe public key
+STRIPE_SECRET_KEY = os.environ["STRIPE_SECRET_KEY"]
+STRIPE_YEARLY_PLAN = os.environ["STRIPE_YEARLY_PLAN"]
+STRIPE_MONTHLY_PLAN = os.environ["STRIPE_MONTHLY_PLAN"]
 
 # Max number emails user can generate for free plan
-MAX_NB_EMAIL_FREE_PLAN = int(os.environ.get("MAX_NB_EMAIL_FREE_PLAN"))
+MAX_NB_EMAIL_FREE_PLAN = int(os.environ["MAX_NB_EMAIL_FREE_PLAN"])
 
-LYRA_ANALYTICS_ID = os.environ.get("LYRA_ANALYTICS_ID")
+LYRA_ANALYTICS_ID = os.environ["LYRA_ANALYTICS_ID"]
 
 # Used to sign id_token
 OPENID_PRIVATE_KEY_PATH = get_abs_path(os.environ["OPENID_PRIVATE_KEY_PATH"])
@@ -70,12 +70,12 @@ PROMO_CODE = "SIMPLEISBETTER"
 
 WORDS_FILE_PATH = get_abs_path(os.environ["WORDS_FILE_PATH"])
 
-GITHUB_CLIENT_ID = os.environ.get("GITHUB_CLIENT_ID")
-GITHUB_CLIENT_SECRET = os.environ.get("GITHUB_CLIENT_SECRET")
+GITHUB_CLIENT_ID = os.environ["GITHUB_CLIENT_ID"]
+GITHUB_CLIENT_SECRET = os.environ["GITHUB_CLIENT_SECRET"]
 
 
-GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
-GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
+GOOGLE_CLIENT_ID = os.environ["GOOGLE_CLIENT_ID"]
+GOOGLE_CLIENT_SECRET = os.environ["GOOGLE_CLIENT_SECRET"]
 
-FACEBOOK_CLIENT_ID = os.environ.get("FACEBOOK_CLIENT_ID")
-FACEBOOK_CLIENT_SECRET = os.environ.get("FACEBOOK_CLIENT_SECRET")
+FACEBOOK_CLIENT_ID = os.environ["FACEBOOK_CLIENT_ID"]
+FACEBOOK_CLIENT_SECRET = os.environ["FACEBOOK_CLIENT_SECRET"]
