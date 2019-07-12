8e459892f78075a1a1352d540a68dce4285ea91b
Son NK <son@imac-de-son.home>
2019-07-12 11:25:53 +0200

    add config.get_abs_path to handle relative path

--- app/config.py
+++ app/config.py
@@ -4,11 +4,22 @@ import subprocess
 from dotenv import load_dotenv
 
 SHA1 = subprocess.getoutput("git rev-parse HEAD")
+ROOT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+
+def get_abs_path(file_path: str):
+    """append ROOT_DIR for relative path"""
+    # Already absolute path
+    if file_path.startswith("/"):
+        return file_path
+    else:
+        return os.path.join(ROOT_DIR, file_path)
+
 
 config_file = os.environ.get("CONFIG")
 if config_file:
     print("load config file", config_file)
-    load_dotenv(config_file)
+    load_dotenv(get_abs_path(config_file))
 else:
     load_dotenv()
 
@@ -28,9 +39,6 @@ FLASK_SECRET = FLASK_SECRET + SHA1
 
 ENABLE_SENTRY = "ENABLE_SENTRY" in os.environ
 ENV = os.environ.get("ENV")
-
-print("email domain is", EMAIL_DOMAIN)
-
 
 AWS_REGION = "eu-west-3"
 BUCKET = os.environ.get("BUCKET") or "local.sl"
@@ -52,15 +60,15 @@ MAX_NB_EMAIL_FREE_PLAN = int(os.environ.get("MAX_NB_EMAIL_FREE_PLAN"))
 LYRA_ANALYTICS_ID = os.environ.get("LYRA_ANALYTICS_ID")
 
 # Used to sign id_token
-OPENID_PRIVATE_KEY_PATH = os.environ.get("OPENID_PRIVATE_KEY_PATH")
-OPENID_PUBLIC_KEY_PATH = os.environ.get("OPENID_PUBLIC_KEY_PATH")
+OPENID_PRIVATE_KEY_PATH = get_abs_path(os.environ["OPENID_PRIVATE_KEY_PATH"])
+OPENID_PUBLIC_KEY_PATH = get_abs_path(os.environ["OPENID_PUBLIC_KEY_PATH"])
 
 PARTNER_CODES = ["SL2019"]
 
 # Allow user to have 1 year of premium: set the expiration_date to 1 year more
 PROMO_CODE = "SIMPLEISBETTER"
 
-WORDS_FILE_PATH = os.environ.get("WORDS_FILE_PATH")
+WORDS_FILE_PATH = get_abs_path(os.environ["WORDS_FILE_PATH"])
 
 GITHUB_CLIENT_ID = os.environ.get("GITHUB_CLIENT_ID")
 GITHUB_CLIENT_SECRET = os.environ.get("GITHUB_CLIENT_SECRET")
