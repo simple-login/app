fc73267b53a41e9aca9a70015d73b6a742fc1f9a
Son NK <son@imac-de-son.home>
2019-07-23 19:33:09 +0200

    remove website url and icon when creating new app

--- app/developer/views/new_client.py
+++ app/developer/views/new_client.py
@@ -1,23 +1,15 @@
-from io import BytesIO
-
 from flask import request, render_template, redirect, url_for, flash
 from flask_login import current_user, login_required
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField
 from wtforms import StringField, validators
 
-from app import s3
 from app.developer.base import developer_bp
 from app.extensions import db
-from app.log import LOG
-from app.models import Client, File
-from app.utils import random_string
+from app.models import Client
 
 
 class NewClientForm(FlaskForm):
     name = StringField("Name", validators=[validators.DataRequired()])
-    icon = FileField("Icon")
-    home_url = StringField("Home Url")
 
 
 @developer_bp.route("/new_client", methods=["GET", "POST"])
@@ -28,23 +20,11 @@ def new_client():
     if request.method == "POST":
         if form.validate():
             client = Client.create_new(form.name.data, current_user.id)
-            client.home_url = form.home_url.data
             db.session.commit()
+            flash("Your app has been created", "success")
 
-            if form.icon.data:
-                file_path = random_string(30)
-                file = File.create(path=file_path)
-
-                s3.upload_from_bytesio(file_path, BytesIO(form.icon.data.read()))
-
-                db.session.commit()
-                LOG.d("upload file %s to s3", file)
-
-                client.icon_id = file.id
-                db.session.commit()
-
-            flash("New client has been created", "success")
-
-            return redirect(url_for("developer.client_detail", client_id=client.id))
+            return redirect(
+                url_for("developer.handle_step", client_id=client.id, step="step-0")
+            )
 
     return render_template("developer/new_client.html", form=form)
