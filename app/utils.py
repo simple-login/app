10019ba4eb02fedd05d69a832eec68c650d493c4
doanguyen <dungnv2410@gmail.com>
2019-12-26 12:26:47 +0100

    reverse previous commit

--- app/utils.py
+++ app/utils.py
@@ -7,20 +7,19 @@ from unidecode import unidecode
 from .config import WORDS_FILE_PATH
 from .log import LOG
 
-def _word():
-    with open(WORDS_FILE_PATH) as f:
-        LOG.d("load words file: %s", WORDS_FILE_PATH)
-        return f.read().split()
+with open(WORDS_FILE_PATH) as f:
+    LOG.d("load words file: %s", WORDS_FILE_PATH)
+    _words = f.read().split()
 
 
 def random_word():
-    return random.choice(_word())
+    return random.choice(_words)
 
 
 def random_words():
     """Generate a random words. Used to generate user-facing string, for ex email addresses"""
     nb_words = random.randint(2, 3)
-    return "_".join([random.choice(_word()) for _ in range(nb_words)])
+    return "_".join([random.choice(_words) for i in range(nb_words)])
 
 
 def random_string(length=10):
