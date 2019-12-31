37ca4eaf20f2da7cdceb8255e85d8384f1c119d8
doanguyen <dungnv2410@gmail.com>
2019-12-31 11:11:06 +0100

    working on paginate alias log page

--- app/dashboard/views/alias_log.py
+++ app/dashboard/views/alias_log.py
@@ -1,12 +1,14 @@
 from dataclasses import dataclass
 
 import arrow
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, abort
 from flask_login import login_required, current_user
 
 from app.dashboard.base import dashboard_bp
 from app.extensions import db
 from app.models import GenEmail, ForwardEmailLog, ForwardEmail
+
+LIMIT = 3
 
 
 @dataclass
@@ -19,9 +21,10 @@ class AliasLog:
     blocked: bool
 
 
-@dashboard_bp.route("/alias_log/<alias>", methods=["GET"])
+@dashboard_bp.route("/alias_log/<alias>", methods=["GET"], defaults={'page_id': 0})
+@dashboard_bp.route("/alias_log/<alias>/<int:page_id>")
 @login_required
-def alias_log(alias):
+def alias_log(alias, page_id):
     gen_email = GenEmail.get_by(email=alias)
 
     # sanity check
@@ -33,19 +36,23 @@ def alias_log(alias):
         flash("You do not have access to this page", "warning")
         return redirect(url_for("dashboard.index"))
 
+    logs = get_alias_log(gen_email, page_id)
+    last_page = len(logs) < LIMIT  # lightweight pagination without counting all objects
+
     return render_template(
-        "dashboard/alias_log.html", logs=get_alias_log(gen_email), alias=alias
+        "dashboard/alias_log.html", **locals()
     )
 
 
-def get_alias_log(gen_email: GenEmail):
+def get_alias_log(gen_email: GenEmail, page_id=0):
     logs: [AliasLog] = []
 
     q = (
         db.session.query(ForwardEmail, ForwardEmailLog)
-        .filter(ForwardEmail.id == ForwardEmailLog.forward_id)
-        .filter(ForwardEmail.gen_email_id == gen_email.id)
-        .all()
+            .filter(ForwardEmail.id == ForwardEmailLog.forward_id)
+            .filter(ForwardEmail.gen_email_id == gen_email.id)
+            .limit(LIMIT)
+            .offset(page_id * LIMIT)
     )
 
     for fe, fel in q:
