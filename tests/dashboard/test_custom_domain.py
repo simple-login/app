fc22593bf6eeead2b82e026786df72b10f31d48f
Son NK <nguyenkims@hotmail.com>
2020-01-28 15:16:26 +0700

    avoid email loop
    Prevent user from adding a domain that they are using for their personal email.

--- tests/dashboard/test_custom_domain.py
+++ tests/dashboard/test_custom_domain.py
@@ -17,3 +17,22 @@ def test_add_domain_success(flask_client):
 
     assert r.status_code == 200
     assert b"New domain ab.cd is created" in r.data
+
+
+def test_add_domain_same_as_user_email(flask_client):
+    """cannot add domain if user personal email uses this domain"""
+    user = login(flask_client)
+    user.lifetime = True
+    db.session.commit()
+
+    r = flask_client.post(
+        url_for("dashboard.custom_domain"),
+        data={"form-name": "create", "domain": "b.c"},  # user email is a@b.c
+        follow_redirects=True,
+    )
+
+    assert r.status_code == 200
+    assert (
+        b"You cannot add a domain that you are currently using for your personal email"
+        in r.data
+    )
