362032b80371e7e9d49e5f98ed120007c0c4512d
Son NK <nguyenkims@hotmail.com>
2020-02-23 14:09:57 +0700

    full_mailbox user can use mailbox

--- app/dashboard/views/mailbox.py
+++ app/dashboard/views/mailbox.py
@@ -27,7 +27,7 @@ class NewMailboxForm(FlaskForm):
 @dashboard_bp.route("/mailbox", methods=["GET", "POST"])
 @login_required
 def mailbox_route():
-    if not current_user.can_use_multiple_mailbox:
+    if not current_user.can_use_multiple_mailbox and not current_user.full_mailbox:
         flash("You don't have access to this page, redirect to home page", "warning")
         return redirect(url_for("dashboard.index"))
 
