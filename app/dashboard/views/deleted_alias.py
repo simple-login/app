f74beb2227e2c365e924532f41e79a5b35f522d9
Son NK <nguyenkims@hotmail.com>
2020-02-23 17:36:45 +0700

    Improve deleted alias page

--- app/dashboard/views/deleted_alias.py
+++ app/dashboard/views/deleted_alias.py
@@ -8,6 +8,6 @@ from app.models import DeletedAlias
 @dashboard_bp.route("/deleted_alias", methods=["GET", "POST"])
 @login_required
 def deleted_alias_route():
-    deleted_aliases = DeletedAlias.query.filter_by(user_id=current_user.id)
+    deleted_aliases = DeletedAlias.query.filter_by(user_id=current_user.id).all()
 
     return render_template("dashboard/deleted_alias.html", **locals())
