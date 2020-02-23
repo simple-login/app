571ff03115e20c420d046e929ae08b3b7b53114a
Son NK <nguyenkims@hotmail.com>
2020-02-23 14:04:00 +0700

    handle case where mailbox_email is the same as user email

--- app/dashboard/views/custom_alias.py
+++ app/dashboard/views/custom_alias.py
@@ -83,7 +83,8 @@ def custom_alias():
                     LOG.d("Set alias %s domain to %s", full_alias, custom_domain)
                     gen_email.custom_domain_id = custom_domain.id
 
-                if mailbox_email != current_user.email:
+                # assign alias to a mailbox
+                if current_user.full_mailbox or mailbox_email != current_user.email:
                     mailbox = Mailbox.get_by(email=mailbox_email)
                     gen_email.mailbox_id = mailbox.id
                     LOG.d("Set alias %s mailbox to %s", full_alias, mailbox)
