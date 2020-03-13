a9fdfc799f3064d1a3121f7d593b12cbfe65c9b1
Son NK <son@simplelogin.io>
2020-03-13 14:37:48 +0100

    Add LOCAL_FILE_UPLOAD param

--- app/s3.py
+++ app/s3.py
@@ -1,22 +1,41 @@
 from io import BytesIO
+import os
 
 import boto3
 import requests
 
-from app.config import AWS_REGION, BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
-
-_session = boto3.Session(
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-    region_name=AWS_REGION,
+from app.config import (
+    AWS_REGION,
+    BUCKET,
+    AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY,
+    LOCAL_FILE_UPLOAD,
+    UPLOAD_DIR,
+    URL,
 )
 
-
-def upload_from_bytesio(key: str, bs: BytesIO, content_type="string") -> None:
-    bs.seek(0)
-    _session.resource("s3").Bucket(BUCKET).put_object(
-        Key=key, Body=bs, ContentType=content_type
+if not LOCAL_FILE_UPLOAD:
+    _session = boto3.Session(
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        region_name=AWS_REGION,
     )
+
+
+def upload_from_bytesio(key: str, bs: BytesIO, content_type="string"):
+    bs.seek(0)
+
+    if LOCAL_FILE_UPLOAD:
+        file_path = os.path.join(UPLOAD_DIR, key)
+        file_dir = os.path.dirname(file_path)
+        os.makedirs(file_dir, exist_ok=True)
+        with open(file_path, "wb") as f:
+            f.write(bs.read())
+
+    else:
+        _session.resource("s3").Bucket(BUCKET).put_object(
+            Key=key, Body=bs, ContentType=content_type
+        )
 
 
 def upload_from_url(url: str, upload_path):
@@ -24,15 +43,13 @@ def upload_from_url(url: str, upload_path):
     upload_from_bytesio(upload_path, BytesIO(r.content))
 
 
-def delete_file(key: str) -> None:
-    o = _session.resource("s3").Bucket(BUCKET).Object(key)
-    o.delete()
-
-
 def get_url(key: str, expires_in=3600) -> str:
-    s3_client = _session.client("s3")
-    return s3_client.generate_presigned_url(
-        ExpiresIn=expires_in,
-        ClientMethod="get_object",
-        Params={"Bucket": BUCKET, "Key": key},
-    )
+    if LOCAL_FILE_UPLOAD:
+        return URL + "/static/upload/" + key
+    else:
+        s3_client = _session.client("s3")
+        return s3_client.generate_presigned_url(
+            ExpiresIn=expires_in,
+            ClientMethod="get_object",
+            Params={"Bucket": BUCKET, "Key": key},
+        )
