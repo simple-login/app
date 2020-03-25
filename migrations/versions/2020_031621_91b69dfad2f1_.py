6cc2f9750fba8b2844e85e7e787fc60e087ad039
Son NK <son@simplelogin.io>
2020-03-25 11:55:35 +0100

    Fix migration (not compatible with master)

--- migrations/versions/2020_031621_91b69dfad2f1_.py
+++ migrations/versions/2020_031621_91b69dfad2f1_.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "91b69dfad2f1"
-down_revision = "9081f1a90939"
+down_revision = "628a5438295c"
 branch_labels = None
 depends_on = None
 
