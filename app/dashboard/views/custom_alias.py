281331bc51e09903de669d0dc71867289ad0d6e7
Son NK <>
2020-05-21 20:05:54 +0200

    use warning level for "Alias creation time expired" error

--- app/dashboard/views/custom_alias.py
+++ app/dashboard/views/custom_alias.py
@@ -83,7 +83,7 @@ def custom_alias():
         try:
             alias_suffix = signer.unsign(signed_suffix, max_age=600).decode()
         except SignatureExpired:
-            LOG.error("Alias creation time expired for %s", current_user)
+            LOG.warning("Alias creation time expired for %s", current_user)
             flash("Alias creation time is expired, please retry", "warning")
             return redirect(url_for("dashboard.custom_alias"))
         except Exception:
