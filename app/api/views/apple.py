d59e9a6238cf5ddfe3dd4f4645de5fb4ab20a440
Son NK <>
2020-07-04 18:24:34 +0200

    use warning level for apple server issue

--- app/api/views/apple.py
+++ app/api/views/apple.py
@@ -303,7 +303,7 @@ def verify_receipt(receipt_data, user, password) -> Optional[AppleSubscription]:
         _PROD_URL, json={"receipt-data": receipt_data, "password": password}
     )
     if r.status_code >= 500:
-        LOG.error("Apple server error, response:%s %s", r, r.content)
+        LOG.warning("Apple server error, response:%s %s", r, r.content)
         return None
 
     if r.json() == {"status": 21007}:
