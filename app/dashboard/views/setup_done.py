6629b8687b0c09e856c41aca5c4c97c1f442d77d
Son NK <>
2020-09-01 20:47:57 +0200

    set cookie setup_done=true on the setup_done page

--- app/dashboard/views/setup_done.py
+++ app/dashboard/views/setup_done.py
@@ -1,10 +1,23 @@
-from flask import render_template
+import arrow
+from flask import render_template, make_response
 from flask_login import login_required
 
+from app.config import URL
 from app.dashboard.base import dashboard_bp
 
 
 @dashboard_bp.route("/setup_done", methods=["GET", "POST"])
 @login_required
 def setup_done():
-    return render_template("dashboard/setup_done.html")
+    response = make_response(render_template("dashboard/setup_done.html"))
+
+    response.set_cookie(
+        "setup_done",
+        value="true",
+        expires=arrow.now().shift(days=30).datetime,
+        secure=True if URL.startswith("https") else False,
+        httponly=True,
+        samesite="Lax",
+    )
+
+    return response
