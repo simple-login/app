6ed6218895d30c03bf55296158f7deebaa90408e
Son NK <>
2020-11-17 09:27:45 +0100

    black

--- app/dashboard/views/alias_log.py
+++ app/dashboard/views/alias_log.py
@@ -84,7 +84,7 @@ def get_alias_log(alias: Alias, page_id=0) -> [AliasLog]:
             blocked=email_log.blocked,
             bounced=email_log.bounced,
             email_log=email_log,
-            contact=contact
+            contact=contact,
         )
         logs.append(al)
     logs = sorted(logs, key=lambda l: l.when, reverse=True)
