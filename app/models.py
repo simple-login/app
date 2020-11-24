6b07be5677ddeea73295f6b0697a63fa43b2324f
Son NK <>
2020-11-24 16:35:16 +0100

    add email_log.mailbox_id column

--- app/models.py
+++ app/models.py
@@ -1335,6 +1335,12 @@ class EmailLog(db.Model, ModelMixin):
         db.ForeignKey("refused_email.id", ondelete="SET NULL"), nullable=True
     )
 
+    # in forward phase, this is the mailbox that will receive the email
+    # in reply phase, this is the mailbox (or a mailbox's authorized address) that sends the email
+    mailbox_id = db.Column(
+        db.ForeignKey("mailbox.id", ondelete="cascade"), nullable=True
+    )
+
     # in case of bounce, record on what mailbox the email has been bounced
     # useful when an alias has several mailboxes
     bounced_mailbox_id = db.Column(
