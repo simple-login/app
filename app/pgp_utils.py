bb8c9451c40e954b160e217d418ea8f443cf5924
Son NK <>
2020-11-25 14:31:14 +0100

    catch all exception in load_public_key()

--- app/pgp_utils.py
+++ app/pgp_utils.py
@@ -21,8 +21,8 @@ class PGPException(Exception):
 
 def load_public_key(public_key: str) -> str:
     """Load a public key into keyring and return the fingerprint. If error, raise Exception"""
-    import_result = gpg.import_keys(public_key)
     try:
+        import_result = gpg.import_keys(public_key)
         return import_result.fingerprints[0]
     except Exception as e:
         raise PGPException("Cannot load key") from e
