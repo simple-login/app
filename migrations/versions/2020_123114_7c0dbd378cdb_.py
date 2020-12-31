0cf283089d70f7d02c7943c598352fd54fb9e481
Son NK <>
2020-12-31 14:46:38 +0100

    fix migration name too long

--- migrations/versions/2020_123114_7c0dbd378cdb_.py
+++ migrations/versions/2020_123114_7c0dbd378cdb_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: rename_default_alias_public_domain_id
+Revision ID: 7c0dbd378cdb
 Revises: f66ca777f409
-Create Date: 2020-12-31 14:11:45.429299
+Create Date: 2020-12-31 14:46:17.781348
 
 """
 import sqlalchemy_utils
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'rename_default_alias_public_domain_id'
+revision = '7c0dbd378cdb'
 down_revision = 'f66ca777f409'
 branch_labels = None
 depends_on = None
@@ -24,3 +24,4 @@ def upgrade():
 
 def downgrade():
     op.alter_column('users', 'default_alias_public_domain_id', new_column_name='default_random_alias_public_domain_id')
+
