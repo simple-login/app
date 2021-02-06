10b6b377ae8a612ba788f658623bcfa544d30b5b
doanguyen <dungnv2410@gmail.com>
2021-02-06 14:48:03 +0100

    refactor(config): remove python eval evil
    Used literal_eval instead of eval.
    The literal_eval in fact is used with warning since it can corrupt the
    interpreter, so we wrapped in a try/catch

--- app/config.py
+++ app/config.py
@@ -4,8 +4,10 @@ import socket
 import string
 import subprocess
 from urllib.parse import urlparse
+from typing import List, Tuple, Optional, Any, Callable
 
 from dotenv import load_dotenv
+from ast import literal_eval
 
 SHA1 = subprocess.getoutput("git rev-parse HEAD")
 ROOT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
@@ -18,6 +20,39 @@ def get_abs_path(file_path: str):
         return file_path
     else:
         return os.path.join(ROOT_DIR, file_path)
+
+
+def parse_environment(env_var: str) -> Optional[Any]:
+    """Convert environmental string into Python object
+
+    Args:
+        env_var (str): environment constant, example: '["domain1.org", "domain2.com"]'
+
+    Returns:
+        Any
+    """
+    try:
+        return literal_eval(env_var)
+    except Exception as e:
+        print("This expression is failed to parsed: %s, with reason: %s", (env_var, e))
+
+
+def sl_get_env(key: str, default_factory: Optional[Callable] = None) -> Any:
+    """Convert environment key into Python object
+
+    Args:
+        key (str): environment constant, example: SL_DB
+        default_factory (Callable, optional): returns value if environment is None. Notice: if this argument and environment variable are not specified, this function will raised. Defaults to None.
+
+    Returns:
+        Any: Python object
+    Raises:
+        ValueError: when key value is empty and default_factory is not specified
+    """
+    env = os.getenv(key)
+    if not (env or default_factory):
+        raise ValueError("This env should be specified: %s", key)
+    return parse_environment(env) or default_factory()
 
 
 config_file = os.environ.get("CONFIG")
@@ -35,7 +70,8 @@ COLOR_LOG = "COLOR_LOG" in os.environ
 PROMO_CODE = "SIMPLEISBETTER"
 
 # Debug mode
-DEBUG = os.environ["DEBUG"] if "DEBUG" in os.environ else False
+DEBUG = bool(os.environ.get("DEBUG", default=False))
+
 # Server url
 URL = os.environ["URL"]
 print(">>> URL:", URL)
@@ -50,8 +86,8 @@ SENTRY_FRONT_END_DSN = os.environ.get("SENTRY_FRONT_END_DSN") or SENTRY_DSN
 
 # Email related settings
 NOT_SEND_EMAIL = "NOT_SEND_EMAIL" in os.environ
-EMAIL_DOMAIN = os.environ["EMAIL_DOMAIN"].lower()
-SUPPORT_EMAIL = os.environ["SUPPORT_EMAIL"]
+EMAIL_DOMAIN = os.environ.get("EMAIL_DOMAIN").lower()
+SUPPORT_EMAIL = os.environ.get("SUPPORT_EMAIL")
 SUPPORT_NAME = os.environ.get("SUPPORT_NAME", "Son from SimpleLogin")
 ADMIN_EMAIL = os.environ.get("ADMIN_EMAIL")
 
@@ -101,43 +137,36 @@ if "POSTFIX_PORT_FORWARD" in os.environ:
 # Useful when calling Postfix from an external network
 POSTFIX_SUBMISSION_TLS = "POSTFIX_SUBMISSION_TLS" in os.environ
 
-if "OTHER_ALIAS_DOMAINS" in os.environ:
-    OTHER_ALIAS_DOMAINS = eval(
-        os.environ["OTHER_ALIAS_DOMAINS"]
-    )  # ["domain1.com", "domain2.com"]
-else:
-    OTHER_ALIAS_DOMAINS = []
+OTHER_ALIAS_DOMAINS = sl_get_env(
+    "OTHER_ALIAS_DOMAINS", default_factory=list
+)  # ["domain1.com", "domain2.com"]
 
 # List of domains user can use to create alias
 if "ALIAS_DOMAINS" in os.environ:
-    ALIAS_DOMAINS = eval(os.environ["ALIAS_DOMAINS"])  # ["domain1.com", "domain2.com"]
+    ALIAS_DOMAINS = os.environ.get("ALIAS_DOMAINS", default="").split(
+        ","
+    )  # ["domain1.com", "domain2.com"]
 else:
     ALIAS_DOMAINS = OTHER_ALIAS_DOMAINS + [EMAIL_DOMAIN]
 
 ALIAS_DOMAINS = [d.lower().strip() for d in ALIAS_DOMAINS]
 
-if "PREMIUM_ALIAS_DOMAINS" in os.environ:
-    PREMIUM_ALIAS_DOMAINS = eval(
-        os.environ["PREMIUM_ALIAS_DOMAINS"]
-    )  # ["domain1.com", "domain2.com"]
-else:
-    PREMIUM_ALIAS_DOMAINS = []
+PREMIUM_ALIAS_DOMAINS: List[str] = sl_get_env(
+    "PREMIUM_ALIAS_DOMAINS", default_factory=list
+)  # ["domain1.com", "domain2.com"]
 
-PREMIUM_ALIAS_DOMAINS = [d.lower().strip() for d in PREMIUM_ALIAS_DOMAINS]
+PREMIUM_ALIAS_DOMAINS = [d.lower().strip() for d in PREMIUM_ALIAS_DOMAINS if d]
 
 # the alias domain used when creating the first alias for user
 FIRST_ALIAS_DOMAIN = os.environ.get("FIRST_ALIAS_DOMAIN") or EMAIL_DOMAIN
 
 # list of (priority, email server)
-EMAIL_SERVERS_WITH_PRIORITY = eval(
-    os.environ["EMAIL_SERVERS_WITH_PRIORITY"]
+EMAIL_SERVERS_WITH_PRIORITY: List[Tuple[int, str]] = sl_get_env(
+    "EMAIL_SERVERS_WITH_PRIORITY", default_factory=list
 )  # [(10, "email.hostname.")]
 
 # these emails are ignored when computing stats
-if os.environ.get("IGNORED_EMAILS"):
-    IGNORED_EMAILS = eval(os.environ.get("IGNORED_EMAILS"))
-else:
-    IGNORED_EMAILS = []
+IGNORED_EMAILS = os.environ.get("IGNORED_EMAILS", default="").split(",")
 
 # disable the alias suffix, i.e. the ".random_word" part
 DISABLE_ALIAS_SUFFIX = "DISABLE_ALIAS_SUFFIX" in os.environ
@@ -196,17 +225,13 @@ except (KeyError, ValueError):
     PADDLE_YEARLY_PRODUCT_ID = -1
 
 # Other Paddle product IDS
-if "PADDLE_MONTHLY_PRODUCT_IDS" in os.environ:
-    PADDLE_MONTHLY_PRODUCT_IDS = eval(os.environ["PADDLE_MONTHLY_PRODUCT_IDS"])
-else:
-    PADDLE_MONTHLY_PRODUCT_IDS = []
+PADDLE_MONTHLY_PRODUCT_IDS = sl_get_env('PADDLE_MONTHLY_PRODUCT_IDS', default_factory=list)
 
 PADDLE_MONTHLY_PRODUCT_IDS.append(PADDLE_MONTHLY_PRODUCT_ID)
 
-if "PADDLE_YEARLY_PRODUCT_IDS" in os.environ:
-    PADDLE_YEARLY_PRODUCT_IDS = eval(os.environ["PADDLE_YEARLY_PRODUCT_IDS"])
-else:
-    PADDLE_YEARLY_PRODUCT_IDS = []
+PADDLE_YEARLY_PRODUCT_IDS = os.environ.get(
+    "PADDLE_YEARLY_PRODUCT_IDS", default=""
+).split(",")
 
 PADDLE_YEARLY_PRODUCT_IDS.append(PADDLE_YEARLY_PRODUCT_ID)
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 from app.utils import random_string, random_words
+import pytest
 
 
 def test_random_words():
@@ -9,3 +10,30 @@ def test_random_words():
 def test_random_string():
     s = random_string()
     assert len(s) > 0
+
+
+def test_parse_environment():
+    from app.config import parse_environment
+
+    array = parse_environment('["word_1", "word_2"]')
+    assert array == ["word_1", "word_2"]
+    env_tuple = parse_environment('("word_1", "word_2")')
+    assert env_tuple == ("word_1", "word_2")
+    env_list_tuple = parse_environment('[(10, "domain_1")]')
+    assert [(10, "domain_1")] == env_list_tuple
+    failed_python_object = parse_environment("a")
+    assert failed_python_object is None
+
+
+def test_sl_get_env(monkeypatch):
+    from app.config import sl_get_env
+
+    monkeypatch.setenv("SL_KEY_1", '["domain_1"]')
+    key_1 = sl_get_env("SL_KEY_1")
+    assert key_1 == ["domain_1"]
+
+    key_2_empty = sl_get_env("SL_KEY_2", default_factory=list)
+    assert key_2_empty == []
+
+    with pytest.raises(ValueError):
+        sl_get_env("SL_KEY_3")
