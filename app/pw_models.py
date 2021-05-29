586654e08e6f401699ea0771a2d7c413efa69b70
nicoo <nicoo@mur.at>
2021-05-29 16:22:47 +0200

    app.pw_models: Refactor, use constant-time equality

--- app/pw_models.py
+++ app/pw_models.py
@@ -9,20 +9,16 @@ _NORMALIZATION_FORM = "NFKC"
 
 
 class PasswordOracle:
-    salt = db.Column(db.String(128), nullable=True)
     password = db.Column(db.String(128), nullable=True)
 
     def set_password(self, password):
         password = unicodedata.normalize(_NORMALIZATION_FORM, password)
         salt = bcrypt.gensalt()
-        password_hash = bcrypt.hashpw(password.encode(), salt).decode()
-        self.salt = salt.decode()
-        self.password = password_hash
+        self.password = bcrypt.hashpw(password.encode(), salt).decode()
 
     def check_password(self, password) -> bool:
         if not self.password:
             return False
 
         password = unicodedata.normalize(_NORMALIZATION_FORM, password)
-        password_hash = bcrypt.hashpw(password.encode(), self.salt.encode())
-        return self.password.encode() == password_hash
+        return bcrypt.checkpw(password.encode(), self.password.encode())
