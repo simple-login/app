6087057c731e3d4b30289144d261537ac9b676c8
alungyutfong <alungyutfong@twitter.com>
2021-06-28 18:38:42 +0200

    test cases for suggest_prefix

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from app.utils import random_string, random_words
+from app.utils import random_string, random_words, suggest_prefix
 
 
 def test_random_words():
@@ -9,3 +9,21 @@ def test_random_words():
 def test_random_string():
     s = random_string()
     assert len(s) > 0
+
+def test_suggest_prefix():
+    case_0 = "github.com"
+    expected_0 = "github"
+
+    case_1 = "www.simplelogin.io"
+    expected_1 = "simplelogin"
+
+    case_co_0 = "bbc.co.uk"
+    expected_co_0 = "bbc"
+
+    case_co_1 = "www.co.fr"
+    expected_co_1 = "co"
+
+    assert suggest_prefix(case_0) == expected_0
+    assert suggest_prefix(case_1) == expected_1
+    assert suggest_prefix(case_co_0) == expected_co_0
+    assert suggest_prefix(case_co_1) == expected_co_1
