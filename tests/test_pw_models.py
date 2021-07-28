b257dddaee92ff067bb084cad91e270649ec0580
nicoo <nicoo@mur.at>
2021-07-28 20:43:26 +0200

    tests/pw_models: property-based test of XChacha20

--- tests/test_pw_models.py
+++ tests/test_pw_models.py
@@ -1,6 +1,8 @@
+from hypothesis import assume, given, strategies as st
 from pytest import raises
+import nacl
 
-from app.pw_models import KeyedOracle
+from app.pw_models import KeyedOracle, UnkeyedOracle, XChacha20
 
 
 class DummyKeyed(KeyedOracle):
@@ -10,7 +12,6 @@ class DummyKeyed(KeyedOracle):
 
     def check(self, user: int, password: bytes, blob: bytes) -> bool:
         raise NotImplementedError
-
 
 class Keyed1(DummyKeyed): pass
 class Keyed2(DummyKeyed): pass
@@ -30,3 +31,33 @@ def test_methods_must_override():
     with raises(TypeError, match="Can't instantiate abstract class.* with abstract method"):
         Unkeyed()
 
+
+class DummyUnkeyed(UnkeyedOracle):
+    """A terrible, dummy implementation of an unkeyed, password-checking oracle.
+
+    This has a host of issues, DO NOT USE.
+    """
+
+    def new(self, _user: int, password: bytes) -> bytes:
+        return password
+
+    def check(self, _user: int, password: bytes, blob: bytes) -> bool:
+        return password == blob
+
+
+def users():
+    """Hypothesis strategy for valid user IDs."""
+    return st.integers(min_value=0, max_value=2**64 - 1)
+
+
+@given(user_1=users(), user_2=users(), password=st.binary())
+def test_xchacha20(user_1: int, user_2: int, password: bytes):
+    assume(user_1 != user_2)
+    oracle = XChacha20(DummyUnkeyed(), 0)  # type: ignore
+    blob = oracle.new(user_1, password)
+
+    # Check that the password is accepted for user_1
+    assert oracle.check(user_1, password, blob)
+    with raises(nacl.exceptions.CryptoError):
+        # Decryption fails if trying to use this record for user_2
+        oracle.check(user_2, password, blob)
