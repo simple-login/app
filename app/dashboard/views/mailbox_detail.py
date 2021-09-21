18acfd9a42714b5c17f86893bc902c4ed05bdfd5
Son Nguyen Kim <nguyenkims@users.noreply.github.com>
2021-09-21 11:19:22 +0200

    handle case mailbox new address is already used

--- app/dashboard/views/mailbox_detail.py
+++ app/dashboard/views/mailbox_detail.py
@@ -263,6 +263,12 @@ def mailbox_confirm_change_route():
 
         # new_email can be None if user cancels change in the meantime
         if mailbox and mailbox.new_email:
+            if Mailbox.get_by(email=mailbox.new_email):
+                flash(f"{mailbox.new_email} is already used", "error")
+                return redirect(
+                    url_for("dashboard.mailbox_detail_route", mailbox_id=mailbox.id)
+                )
+
             mailbox.email = mailbox.new_email
             mailbox.new_email = None
 
