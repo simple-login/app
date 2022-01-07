d4e31257faae5989bd1624daa30bda6340ba0dd1
Son <nguyenkims@users.noreply.github.com>
2022-01-07 16:45:12 +0100

    make sure to output exception name in log

--- app/errors.py
+++ app/errors.py
@@ -1,52 +1,58 @@
-class AliasInTrashError(Exception):
+class SLException(Exception):
+    def __str__(self):
+        super_str = super().__str__()
+        return f"{type(self).__name__} {super_str}"
+
+
+class AliasInTrashError(SLException):
     """raised when alias is deleted before """
 
     pass
 
 
-class DirectoryInTrashError(Exception):
+class DirectoryInTrashError(SLException):
     """raised when a directory is deleted before """
 
     pass
 
 
-class SubdomainInTrashError(Exception):
+class SubdomainInTrashError(SLException):
     """raised when a subdomain is deleted before """
 
     pass
 
 
-class CannotCreateContactForReverseAlias(Exception):
+class CannotCreateContactForReverseAlias(SLException):
     """raised when a contact is created that has website_email=reverse_alias of another contact"""
 
     pass
 
 
-class NonReverseAliasInReplyPhase(Exception):
+class NonReverseAliasInReplyPhase(SLException):
     """raised when a non reverse-alias is used during a reply phase"""
 
     pass
 
 
-class VERPTransactional(Exception):
+class VERPTransactional(SLException):
     """raised an email sent to a transactional VERP can't be handled"""
 
     pass
 
 
-class VERPForward(Exception):
+class VERPForward(SLException):
     """raised an email sent to a forward VERP can't be handled"""
 
     pass
 
 
-class VERPReply(Exception):
+class VERPReply(SLException):
     """raised an email sent to a reply VERP can't be handled"""
 
     pass
 
 
-class MailSentFromReverseAlias(Exception):
+class MailSentFromReverseAlias(SLException):
     """raised when receiving an email sent from a reverse alias"""
 
     pass
