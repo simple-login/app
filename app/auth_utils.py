5c2c082ed929e44751807e4bbd0c84280e16ebd3
george <george@fozzie.dev>
2022-01-21 19:55:57 +0000

    Add logging and return on timeout.

--- app/auth_utils.py
+++ app/auth_utils.py
@@ -1,6 +1,7 @@
 import requests
 import hashlib
 from app.config import DISABLE_PWNEDPASSWORDS
+from app.log import LOG
 
 
 def check_pwnedpasswords(password, bypass=False):
@@ -14,17 +15,28 @@ def check_pwnedpasswords(password, bypass=False):
     else:
         sha1_hash = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
         lookup_hash = sha1_hash[0:5]
-        pwnedpasswords_res = requests.get(
-            "https://api.pwnedpasswords.com/range/" + lookup_hash,
-            headers={"Add-Padding": "true"},
-            timeout=10,
-        ).content.decode("utf-8")
-        unpad_matches = list(
-            filter(lambda x: int(x.split(":")[1]) > 0, pwnedpasswords_res.splitlines())
-        )
-
-        found = list(filter(lambda x: sha1_hash[5:] == x.split(":")[0], unpad_matches))
-        if len(found) > 0:
-            return found[0].split(":")[1]
-        else:
+        try:
+            pwnedpasswords_res = requests.get(
+                "https://api.pwnedpasswords.com/range/" + lookup_hash,
+                headers={"Add-Padding": "true"},
+                timeout=10,
+            ).content.decode("utf-8")
+        except requests.exceptions.Timeout:
+            LOG.w("Pwned Passwords timeout")
             return False
+        except Exception as e:
+            LOG.w("Pwned Passwords error %s", e)
+        else:
+            unpad_matches = list(
+                filter(
+                    lambda x: int(x.split(":")[1]) > 0, pwnedpasswords_res.splitlines()
+                )
+            )
+
+            found = list(
+                filter(lambda x: sha1_hash[5:] == x.split(":")[0], unpad_matches)
+            )
+            if len(found) > 0:
+                return found[0].split(":")[1]
+            else:
+                return False
