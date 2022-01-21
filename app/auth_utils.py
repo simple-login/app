8e95847b8b4464eb43ceffbe360edeac613dc82a
george <george@fozzie.dev>
2022-01-21 16:02:01 +0000

    Add support for padding.

--- app/auth_utils.py
+++ app/auth_utils.py
@@ -15,9 +15,13 @@ def check_pwnedpasswords(password, bypass=False):
         sha1_hash = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
         lookup_hash = sha1_hash[0:5]
         pwnedpasswords_res = requests.get(
-            "https://api.pwnedpasswords.com/range/" + lookup_hash
+            "https://api.pwnedpasswords.com/range/" + lookup_hash,
+            headers={'Add-Padding': 'True'},
         ).content.decode("utf-8")
+        unpad_matches = list(
+            filter(lambda x: int(x.split(":")[1]) > 0, pwnedpasswords_res.splitlines())
+        )
         hash_matches = list(
-            map(lambda x: x.split(":")[0], pwnedpasswords_res.splitlines())
+            map(lambda x: x.split(":")[0], unpad_matches)
         )
         return sha1_hash[5:] in hash_matches
