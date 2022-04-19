b545ebaeb117e95ed5f16b497c16cb82a8a35414
Son <nguyenkims@users.noreply.github.com>
2022-04-19 19:40:50 +0200

    remove unnecessary test

--- tests/auth/test_login.py
+++ tests/auth/test_login.py
@@ -20,16 +20,3 @@ def test_unactivated_user_login(flask_client):
         b"Please check your inbox for the activation email. You can also have this email re-sent"
         in r.data
     )
-
-
-def test_activated_user_login(flask_client):
-    user = create_new_user()
-
-    r = flask_client.post(
-        url_for("auth.login"),
-        data={"email": user.email, "password": "password"},
-        follow_redirects=True,
-    )
-
-    assert r.status_code == 200
-    assert b"/auth/logout" in r.data
