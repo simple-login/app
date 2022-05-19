e6acff13e57858491c569e1a9654d9976dab8791
Carlos Quintana <carlos.quintana@proton.ch>
2022-05-19 11:47:22 +0200

    Send extension setup message if user is logged in

--- app/onboarding/views/index.py
+++ app/onboarding/views/index.py
@@ -1,9 +1,10 @@
 from app.onboarding.base import onboarding_bp
 from flask import render_template
+from flask_login import current_user
 
 
 @onboarding_bp.route("/", methods=["GET"])
 def index():
     return render_template(
-        "onboarding/index.html",
+        "onboarding/index.html", is_user_logged_in=current_user is not None
     )
