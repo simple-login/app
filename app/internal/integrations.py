6d86e64d65ded718d864ca43f12742e439c6a340
Son Nguyen Kim <nguyenkims@users.noreply.github.com>
2022-07-04 15:39:12 +0200

    show msg on /internal/integrations/proton (#1139)
    * show msg on /internal/integrations/proton
    
    * highlight the connect with Proton section
    
    * djlint

--- app/internal/integrations.py
+++ app/internal/integrations.py
@@ -1,6 +1,6 @@
 import arrow
 from app.config import CONNECT_WITH_PROTON_COOKIE_NAME, URL
-from flask import make_response, redirect, url_for
+from flask import make_response, redirect, url_for, flash
 from flask_login import current_user
 from .base import internal_bp
 
@@ -8,12 +8,13 @@ from .base import internal_bp
 @internal_bp.route("/integrations/proton")
 def set_enable_proton_cookie():
     if current_user.is_authenticated:
-        redirect_url = url_for("dashboard.index")
+        redirect_url = url_for("dashboard.setting", _anchor="connect-with-proton")
     else:
         redirect_url = url_for("auth.login")
 
     response = make_response(redirect(redirect_url))
     if CONNECT_WITH_PROTON_COOKIE_NAME:
+        flash("You can now connect your Proton and your SimpleLogin account", "success")
         response.set_cookie(
             CONNECT_WITH_PROTON_COOKIE_NAME,
             value="true",
