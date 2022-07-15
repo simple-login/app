bcd4383e0582abb52d02fb9fc42d82a936be32e7
Adrià Casajús <acasajus@users.noreply.github.com>
2022-07-15 17:48:42 +0200

    Sanitize the highlight contact id (#1160)
    * Sanitize also parameter
    
    * Formatting
    
    Co-authored-by: Adrià Casajús <adria.casajus@proton.ch>

--- app/dashboard/views/alias_contact_manager.py
+++ app/dashboard/views/alias_contact_manager.py
@@ -234,7 +234,11 @@ def delete_contact(alias: Alias, contact_id: int):
 def alias_contact_manager(alias_id):
     highlight_contact_id = None
     if request.args.get("highlight_contact_id"):
-        highlight_contact_id = int(request.args.get("highlight_contact_id"))
+        try:
+            highlight_contact_id = int(request.args.get("highlight_contact_id"))
+        except ValueError:
+            flash("Invalid contact id", "error")
+            return redirect(url_for("dashboard.index"))
 
     alias = Alias.get(alias_id)
 
