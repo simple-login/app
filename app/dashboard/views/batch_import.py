8f3b956c5c795a2393288df809a24d8307b5448b
Adrià Casajús <adria.casajus@proton.ch>
2022-07-15 18:58:44 +0200

    Limit amount of imports

--- app/dashboard/views/batch_import.py
+++ app/dashboard/views/batch_import.py
@@ -28,6 +28,16 @@ def batch_import_route():
     batch_imports = BatchImport.filter_by(user_id=current_user.id).all()
 
     if request.method == "POST":
+        total_imports = BatchImport.filter_by(user_id=current_user.id).count()
+        if total_imports > 10:
+            flash(
+                "You have too many imports already. Wait until some get cleaned up",
+                "error",
+            )
+            return render_template(
+                "dashboard/batch_import.html", batch_imports=batch_imports
+            )
+
         alias_file = request.files["alias-file"]
 
         file_path = random_string(20) + ".csv"
