9e2c0232e44540c7f05f3891957ebec951c0f1a7
Son <nguyenkims@users.noreply.github.com>
2022-07-26 09:44:25 +0200

    remove envs used for proton beta link

--- app/internal/integrations.py
+++ app/internal/integrations.py
@@ -1,8 +1,7 @@
-import arrow
 from flask import make_response, redirect, url_for, flash
 from flask_login import current_user
+
 from .base import internal_bp
-from app import config
 
 
 @internal_bp.route("/integrations/proton")
@@ -13,14 +12,6 @@ def set_enable_proton_cookie():
         redirect_url = url_for("auth.login")
 
     response = make_response(redirect(redirect_url))
-    if config.PROTON_ALLOW_INTERNAL_LINK:
-        flash("You can now connect your Proton and your SimpleLogin account", "success")
-        response.set_cookie(
-            config.CONNECT_WITH_PROTON_COOKIE_NAME,
-            value="true",
-            expires=arrow.now().shift(days=30).datetime,
-            secure=True if config.URL.startswith("https") else False,
-            httponly=True,
-            samesite="Lax",
-        )
+
+    flash("You can now connect your Proton and your SimpleLogin account", "success")
     return response
--- app/proton/utils.py
+++ app/proton/utils.py
@@ -1,8 +1,6 @@
 from typing import Optional
 
-from app.config import CONNECT_WITH_PROTON, CONNECT_WITH_PROTON_COOKIE_NAME
-from flask import request
-
+from app.config import CONNECT_WITH_PROTON
 from app.db import Session
 from app.errors import ProtonPartnerNotSetUp
 from app.models import Partner
@@ -28,9 +26,5 @@ def is_proton_partner(partner: Partner) -> str:
 
 def is_connect_with_proton_enabled() -> bool:
     if CONNECT_WITH_PROTON:
-        return True
-    if CONNECT_WITH_PROTON_COOKIE_NAME and request.cookies.get(
-        CONNECT_WITH_PROTON_COOKIE_NAME
-    ):
         return True
     return False
