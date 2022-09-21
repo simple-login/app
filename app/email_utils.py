553477191fdc8d1d4d2563adf0fc92733686649e
Son <nguyenkims@users.noreply.github.com>
2022-09-21 20:01:38 +0200

    do not add mime-version header if already present

--- app/email_utils.py
+++ app/email_utils.py
@@ -319,7 +319,8 @@ def send_email(
     date_header = formatdate()
     msg[headers.DATE] = date_header
 
-    msg[headers.MIME_VERSION] = "1.0"
+    if headers.MIME_VERSION not in msg:
+        msg[headers.MIME_VERSION] = "1.0"
 
     if unsubscribe_link:
         add_or_replace_header(msg, headers.LIST_UNSUBSCRIBE, f"<{unsubscribe_link}>")
