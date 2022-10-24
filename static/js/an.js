efd3b8d51f9f7f2ab0c65bf288a3e23d921f9749
Son <nguyenkims@users.noreply.github.com>
2022-10-24 18:17:42 +0200

    use /p.outbound.js and /p/api/event on app.sl.io

--- static/js/an.js
+++ static/js/an.js
@@ -13,13 +13,13 @@
   console.log("init Analytics");
 
   // Add Plausible script
-  // <script async defer data-domain="app.simplelogin.io,everything.simplelogin.com" src="https://simplelogin.io/p.outbound.js"></script>
+  // <script async defer data-domain="app.simplelogin.io,everything.simplelogin.com" src="/p.outbound.js"></script>
   var plausibleScript = document.createElement('script');
   plausibleScript.defer = 1;
   plausibleScript.async = 1;
-  plausibleScript.dataset.api = "https://simplelogin.io/p/api/event";
+  plausibleScript.dataset.api = "/p/api/event";
   plausibleScript.dataset.domain = "app.simplelogin.io,everything.simplelogin.com";
-  plausibleScript.src = 'https://simplelogin.io/p.outbound.js';
+  plausibleScript.src = '/p.outbound.js';
 
   var ins = document.getElementsByTagName('script')[0];
   ins.parentNode.insertBefore(plausibleScript, ins);
