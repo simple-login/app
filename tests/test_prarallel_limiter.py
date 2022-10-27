f6463a5adc16d9e124f1df4d90070c213e2e2469
Adrià Casajús <acasajus@users.noreply.github.com>
2022-10-27 10:40:33 +0200

    Change: Do not sleep on exclusive zones (#1375)
    * Change: Do not sleep on exclusive zones
    
    * Update test
    
    Co-authored-by: Adrià Casajús <adria.casajus@proton.ch>

--- tests/test_prarallel_limiter.py
+++ tests/test_prarallel_limiter.py
@@ -8,6 +8,6 @@ def test_parallel_limiter(flask_app):
     user = create_new_user()
     with flask_app.test_request_context():
         login_user(user)
-        pl = _InnerLock("test", max_wait_secs=1, wait_loop_secs=0.1)
+        pl = _InnerLock("test", max_wait_secs=1)
         for loop_id in range(10):
             assert pl(lambda x: x)(loop_id) == loop_id
