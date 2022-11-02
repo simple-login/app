3bc976c3221b0dee0a732eb9e4deb5adec183982
Adrià Casajús <acasajus@users.noreply.github.com>
2022-11-02 15:41:48 +0100

    Feature: Add app name to each db connection (#1393)
    Co-authored-by: Adrià Casajús <adria.casajus@proton.ch>

--- app/db.py
+++ app/db.py
@@ -3,9 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 
-from app.config import DB_URI
+from app import config
 
-engine = create_engine(DB_URI)
+
+engine = create_engine(
+    config.DB_URI, connect_args={"application_name": config.DB_CONN_NAME}
+)
 connection = engine.connect()
 
 Session = scoped_session(sessionmaker(bind=connection))
