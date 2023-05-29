b97d20c09890bf11d67aeee75f28c0b6ff7d4118
Adrià Casajús <adria.casajus@proton.ch>
2023-05-29 16:33:21 +0200

    Added test to validate suffixes

--- tests/test_alias_suffixes.py
+++ tests/test_alias_suffixes.py
@@ -1,6 +1,8 @@
+import re
+
 from app.alias_suffix import get_alias_suffixes
 from app.db import Session
-from app.models import SLDomain, PartnerUser, AliasOptions
+from app.models import SLDomain, PartnerUser, AliasOptions, CustomDomain
 from app.proton.utils import get_proton_partner
 from init_app import add_sl_domains
 from tests.utils import create_new_user, random_token
@@ -56,3 +58,25 @@ def test_get_default_domain_even_if_is_not_allowed():
     )
     suffixes = get_alias_suffixes(user, alias_options=options)
     assert suffixes[0].domain == default_domain.domain
+
+
+def test_suffixes_are_valid():
+    user = create_new_user()
+    PartnerUser.create(
+        partner_id=get_proton_partner().id,
+        user_id=user.id,
+        external_user_id=random_token(10),
+        flush=True,
+    )
+    CustomDomain.create(
+        user_id=user.id, domain=f"{random_token(10)}.com", verified=True
+    )
+    user.trial_end = None
+    Session.flush()
+    options = AliasOptions(
+        show_sl_domains=True, show_partner_domains=get_proton_partner()
+    )
+    alias_suffixes = get_alias_suffixes(user, alias_options=options)
+    valid_re = re.compile(r"^(\.[\w_]+)?@[\.\w]+$")
+    for suffix in alias_suffixes:
+        assert valid_re.match(suffix.suffix)
