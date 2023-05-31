d30183bbdae57365ccd366cc6944023238fc7b96
Carlos Quintana <74399022+cquintana92@users.noreply.github.com>
2023-05-31 09:40:20 +0200

    fix: remove user password from export user data (#1758)

--- app/jobs/export_user_data_job.py
+++ app/jobs/export_user_data_job.py
@@ -41,7 +41,7 @@ from app.models import (
 class ExportUserDataJob:
 
     REMOVE_FIELDS = {
-        "User": ("otp_secret",),
+        "User": ("otp_secret", "password"),
         "Alias": ("ts_vector", "transfer_token", "hibp_last_check"),
         "CustomDomain": ("ownership_txt_token",),
     }
