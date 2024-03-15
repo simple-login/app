e60f67dbb7ffc656e7b9d18a5833978a6c08f921
Adrià Casajús <adria.casajus@proton.ch>
2024-03-15 13:50:35 +0100

    Update

--- tasks/cleanup_old_notifications.py
+++ tasks/cleanup_old_notifications.py
@@ -4,8 +4,6 @@ from app.log import LOG
 from app.models import Notification
 
 
-def cleanup_old_notifications():
-    count = Notification.filter(
-        Notification.created_at < arrow.now().shift(days=-15)
-    ).delete()
+def cleanup_old_notifications(oldest_allowed: arrow.Arrow):
+    count = Notification.filter(Notification.created_at < oldest_allowed).delete()
     LOG.i(f"Deleted {count} notifications")
