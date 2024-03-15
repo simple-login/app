d11b77d13f3acbb8e2c5ffee767b849770d7d80e
Adrià Casajús <adria.casajus@proton.ch>
2024-03-15 13:57:36 +0100

    Added tests

--- tasks/cleanup_old_jobs.py
+++ tasks/cleanup_old_jobs.py
@@ -6,13 +6,16 @@ from app.log import LOG
 from app.models import Job, JobState
 
 
-def cleanup_old_jobs():
+def cleanup_old_jobs(oldest_allowed: arrow.Arrow):
     count = Job.filter(
         or_(
-            Job.state == JobState.done,
-            Job.state == JobState.error,
-            and_(Job.state == JobState.taken, Job.attempts >= config.JOB_MAX_ATTEMPTS),
+            Job.state == JobState.done.value,
+            Job.state == JobState.error.value,
+            and_(
+                Job.state == JobState.taken.value,
+                Job.attempts >= config.JOB_MAX_ATTEMPTS,
+            ),
         ),
-        Job.updated_at < arrow.now().shift(days=-15),
+        Job.updated_at < oldest_allowed,
     ).delete()
     LOG.i(f"Deleted {count} jobs")
