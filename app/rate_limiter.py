ec0a25bdbd17962d3be982874776fb0590ee311d
Adrià Casajús <adria.casajus@proton.ch>
2024-04-08 12:52:43 +0200

    log fixes

--- app/rate_limiter.py
+++ app/rate_limiter.py
@@ -31,7 +31,7 @@ def check_bucket_limit(
         value = lock_redis.incr(bucket_lock_name, bucket_seconds)
         if value > max_hits:
             LOG.i(
-                f"Rate limit hit for {lock_name} (bucket it {bucket_id} -> {value}/{max_hits}"
+                f"Rate limit hit for {lock_name} (bucket id {bucket_id}) -> {value}/{max_hits}"
             )
             newrelic.agent.record_custom_event(
                 "BucketRateLimit",
