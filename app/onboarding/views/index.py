942d73520e2f12397703fac2dabef37aba3cd9fd
Adrià Casajús <adria.casajus@proton.ch>
2024-07-12 16:42:00 +0200

    Disable the enforced header until all extensions are updated and add a fallback option to trigger a manual login

--- app/onboarding/views/index.py
+++ app/onboarding/views/index.py
@@ -1,7 +1,13 @@
 from app.onboarding.base import onboarding_bp
-from flask import render_template
+from flask import render_template, url_for, redirect
 
 
 @onboarding_bp.route("/", methods=["GET"])
 def index():
-    return render_template("onboarding/index.html")
+    # Do the redirect to ensure cookies are set because they are SameSite=lax/strict
+    return redirect(url_for("onboarding.setup"))
+
+
+@onboarding_bp.route("/setup", methods=["GET"])
+def setup():
+    return render_template("onboarding/setup.html")
