9a33249a6af4c4e33f5b07ce7388c41bbe519e57
Adrià Casajús <adria.casajus@proton.ch>
2024-09-09 19:09:37 +0200

    Reorder

--- tests/events/test_event_dispatcher.py
+++ tests/events/test_event_dispatcher.py
@@ -1,100 +1,33 @@
-from app import config, alias_utils
-from app.db import Session
-from app.events.event_dispatcher import GlobalDispatcher
-from app.events.generated import event_pb2
-from app.models import Alias, User, PartnerUser
-from utils import random_token
+from app.events.event_dispatcher import EventDispatcher
+from app.events.generated.event_pb2 import EventContent, UserDeleted
 from .event_test_utils import (
+    _create_unlinked_user,
     OnMemoryDispatcher,
     _create_linked_user,
 )
 
-on_memory_dispatcher = OnMemoryDispatcher()
+
+def test_event_dispatcher_stores_events():
+    dispatcher = OnMemoryDispatcher()
+
+    (user, partner) = _create_linked_user()
+    content = EventContent(user_deleted=UserDeleted())
+    EventDispatcher.send_event(user, content, dispatcher, skip_if_webhook_missing=False)
+    assert len(dispatcher.memory) == 1
+
+    content = EventContent(user_deleted=UserDeleted())
+    EventDispatcher.send_event(user, content, dispatcher, skip_if_webhook_missing=False)
+    assert len(dispatcher.memory) == 2
 
 
-def setup_module():
-    GlobalDispatcher.set_dispatcher(on_memory_dispatcher)
-    config.EVENT_WEBHOOK = "http://test"
+def test_event_dispatcher_does_not_send_event_if_user_not_linked():
+    dispatcher = OnMemoryDispatcher()
 
+    user = _create_unlinked_user()
+    content = EventContent(user_deleted=UserDeleted())
+    EventDispatcher.send_event(user, content, dispatcher, skip_if_webhook_missing=False)
+    assert len(dispatcher.memory) == 0
 
-def teardown_module():
-    GlobalDispatcher.set_dispatcher(None)
-    config.EVENT_WEBHOOK = None
-
-
-def setup_function(func):
-    on_memory_dispatcher.clear()
-
-
-def _get_event_from_string(
-    data: str, user: User, pu: PartnerUser
-) -> event_pb2.EventContent:
-    event = event_pb2.Event()
-    event.ParseFromString(data)
-    assert user.id == event.user_id
-    assert pu.external_user_id == event.external_user_id
-    assert pu.partner_id == event.partner_id
-    return event.content
-
-
-def test_fire_event_on_alias_creation():
-    (user, pu) = _create_linked_user()
-    alias = Alias.create_new_random(user)
-    Session.flush()
-    assert len(on_memory_dispatcher.memory) == 1
-    event_data = on_memory_dispatcher.memory[0]
-    event_content = _get_event_from_string(event_data, user, pu)
-    assert event_content.alias_created is not None
-    alias_created = event_content.alias_created
-    assert alias.id == alias_created.alias_id
-    assert alias.email == alias_created.alias_email
-    assert "" == alias_created.alias_note
-    assert alias.enabled == alias_created.enabled
-
-
-def test_fire_event_on_alias_creation_with_note():
-    (user, pu) = _create_linked_user()
-    note = random_token(10)
-    alias = Alias.create_new_random(user, note=note)
-    Session.flush()
-    assert len(on_memory_dispatcher.memory) == 1
-    event_data = on_memory_dispatcher.memory[0]
-    event_content = _get_event_from_string(event_data, user, pu)
-    assert event_content.alias_created is not None
-    alias_created = event_content.alias_created
-    assert alias.id == alias_created.alias_id
-    assert alias.email == alias_created.alias_email
-    assert note == alias_created.alias_note
-    assert alias.enabled == alias_created.enabled
-
-
-def test_fire_event_on_alias_deletion():
-    (user, pu) = _create_linked_user()
-    alias = Alias.create_new_random(user)
-    alias_id = alias.id
-    Session.flush()
-    on_memory_dispatcher.clear()
-    alias_utils.delete_alias(alias, user)
-    assert len(on_memory_dispatcher.memory) == 1
-    event_data = on_memory_dispatcher.memory[0]
-    event_content = _get_event_from_string(event_data, user, pu)
-    assert event_content.alias_deleted is not None
-    alias_deleted = event_content.alias_deleted
-    assert alias_id == alias_deleted.alias_id
-    assert alias.email == alias_deleted.alias_email
-
-
-def test_fire_event_on_alias_status_change():
-    (user, pu) = _create_linked_user()
-    alias = Alias.create_new_random(user)
-    Session.flush()
-    on_memory_dispatcher.clear()
-    alias_utils.change_alias_status(alias, True)
-    assert len(on_memory_dispatcher.memory) == 1
-    event_data = on_memory_dispatcher.memory[0]
-    event_content = _get_event_from_string(event_data, user, pu)
-    assert event_content.alias_status_change is not None
-    event = event_content.alias_status_change
-    assert alias.id == event.alias_id
-    assert alias.email == event.alias_email
-    assert event.enabled
+    content = EventContent(user_deleted=UserDeleted())
+    EventDispatcher.send_event(user, content, dispatcher, skip_if_webhook_missing=False)
+    assert len(dispatcher.memory) == 0
