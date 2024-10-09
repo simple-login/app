4c321ea8edafafb32a6101b254962e7a37f4b40b
Adrià Casajús <adria.casajus@proton.ch>
2024-10-09 10:53:09 +0200

    Ensure email is sent

--- app/auth/views/login.py
+++ app/auth/views/login.py
@@ -38,6 +38,10 @@ def login():
     show_resend_activation = False
 
     if form.validate_on_submit():
+        email = form.email.data
+        if not email:
+            flash("Email or password incorrect", "error")
+            LoginEvent(LoginEvent.ActionType.failed).send()
         email = sanitize_email(form.email.data)
         canonical_email = canonicalize_email(email)
         user = User.get_by(email=email) or User.get_by(email=canonical_email)
