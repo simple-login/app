23796197f7bdd17473bfd550814c22197a8f26c8
Adrià Casajús <adria.casajus@proton.ch>
2025-01-15 18:33:59 +0100

    Imports in order

--- scripts/regen-words.py
+++ scripts/regen-words.py
@@ -3,11 +3,11 @@ import os
 import sys
 import json
 
-from app import config
-from app.log import LOG
-
 rootDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(rootDir)
+
+from app import config  # noqa: E402
+from app.log import LOG  # noqa: E402
 
 LOG.i(f"Reading {config.WORDS_FILE_PATH} file")
 words = [
@@ -23,11 +23,13 @@ serialized_words = json.dumps(words)
 with open(destFile, "wb") as fd:
     fd.write(
         f"""#
-#This file is auto-generated. Please run {sys.argv[0]} to re-generate it
+# This file is auto-generated. Please run {sys.argv[0]} to re-generate it
 #
 
 import json
 
-safe_words = json.loads('{serialized_words}')
+safe_words = json.loads(
+    '{serialized_words}'
+)
 """.encode("utf-8")
     )
