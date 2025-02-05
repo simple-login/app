ffb070cd191dc39a9ecfde2d623272089e1bef68
Carlos Quintana <74399022+cquintana92@users.noreply.github.com>
2025-02-05 11:09:52 +0100

    chore: offer version in newrelic events (#2380)
    * chore: offer version in newrelic events
    
    * chore: send version event on startup and on other services

--- events/runner.py
+++ events/runner.py
@@ -4,20 +4,24 @@ import newrelic.agent
 from app.log import LOG
 from app.db import Session
 from app.models import SyncEvent
+from app.monitor_utils import send_version_event
 from events.event_sink import EventSink
 from events.event_source import EventSource
 
 
 class Runner:
-    def __init__(self, source: EventSource, sink: EventSink):
+    def __init__(self, source: EventSource, sink: EventSink, service_name: str = ""):
         self.__source = source
         self.__sink = sink
+        self.__service_name = service_name
 
     def run(self):
         self.__source.run(self.__on_event)
 
     @newrelic.agent.background_task()
     def __on_event(self, event: SyncEvent):
+        if self.__service_name:
+            send_version_event(self.__service_name)
         try:
             event_created_at = event.created_at
             start_time = arrow.now()
