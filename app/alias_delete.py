7a707bdb6d8761954b20f9db7e8cc71d079a502f
Carlos Quintana <74399022+cquintana92@users.noreply.github.com>
2025-03-27 16:33:49 +0100

    fix: trash custom domain alias deletes it (#2429)
    * fix: trash custom domain alias deletes it
    
    * chore: remove redundant commits

--- app/alias_delete.py
+++ app/alias_delete.py
@@ -19,6 +19,47 @@ from app.models import (
 )
 
 
+def __delete_alias(alias: Alias, user: User, commit: bool):
+    alias_id = alias.id
+    alias_email = alias.email
+
+    emit_alias_audit_log(
+        alias, AliasAuditLogAction.DeleteAlias, "Alias deleted by user action"
+    )
+    Alias.filter(Alias.id == alias.id).delete()
+
+    EventDispatcher.send_event(
+        user,
+        EventContent(alias_deleted=AliasDeleted(id=alias_id, email=alias_email)),
+    )
+    if commit:
+        Session.commit()
+
+
+def __delete_if_custom_domain(
+    alias: Alias, user: User, reason: AliasDeleteReason, commit: bool
+) -> bool:
+    if not alias.custom_domain_id:
+        return False
+
+    if not DomainDeletedAlias.get_by(
+        email=alias.email, domain_id=alias.custom_domain_id
+    ):
+        domain_deleted_alias = DomainDeletedAlias(
+            user_id=user.id,
+            email=alias.email,
+            domain_id=alias.custom_domain_id,
+            reason=reason,
+        )
+        Session.add(domain_deleted_alias)
+        LOG.i(
+            f"Moving {alias} to domain {alias.custom_domain_id} trash {domain_deleted_alias}"
+        )
+
+    __delete_alias(alias, user, commit)
+    return True
+
+
 def delete_alias(
     alias: Alias,
     user: User,
@@ -49,45 +90,20 @@ def perform_alias_deletion(
     reason: AliasDeleteReason = AliasDeleteReason.Unspecified,
     commit: bool = False,
 ):
-    if alias.custom_domain_id:
-        if not DomainDeletedAlias.get_by(
-            email=alias.email, domain_id=alias.custom_domain_id
-        ):
-            domain_deleted_alias = DomainDeletedAlias(
-                user_id=user.id,
-                email=alias.email,
-                domain_id=alias.custom_domain_id,
-                reason=reason,
-            )
-            Session.add(domain_deleted_alias)
-            Session.commit()
-            LOG.i(
-                f"Moving {alias} to domain {alias.custom_domain_id} trash {domain_deleted_alias}"
-            )
-    else:
-        if not DeletedAlias.get_by(email=alias.email):
-            deleted_alias = DeletedAlias(
-                email=alias.email, reason=alias.delete_reason or reason
-            )
-            Session.add(deleted_alias)
-            Session.commit()
-            LOG.i(f"Moving {alias} to global trash {deleted_alias}")
+    if __delete_if_custom_domain(alias, user, reason, commit):
+        LOG.info(
+            f"Moved alias {alias.id} ({alias.email}) of custom domain {alias.custom_domain_id} to custom domain trash"
+        )
+        return
 
-    alias_id = alias.id
-    alias_email = alias.email
+    if not DeletedAlias.get_by(email=alias.email):
+        deleted_alias = DeletedAlias(
+            email=alias.email, reason=alias.delete_reason or reason
+        )
+        Session.add(deleted_alias)
+        LOG.i(f"Moving {alias} to global trash {deleted_alias}")
 
-    emit_alias_audit_log(
-        alias, AliasAuditLogAction.DeleteAlias, "Alias deleted by user action"
-    )
-    Alias.filter(Alias.id == alias.id).delete()
-    Session.commit()
-
-    EventDispatcher.send_event(
-        user,
-        EventContent(alias_deleted=AliasDeleted(id=alias_id, email=alias_email)),
-    )
-    if commit:
-        Session.commit()
+    __delete_alias(alias, user, commit)
 
 
 def move_alias_to_trash(
@@ -96,6 +112,12 @@ def move_alias_to_trash(
     reason: AliasDeleteReason = AliasDeleteReason.Unspecified,
     commit: bool = False,
 ):
+    if __delete_if_custom_domain(alias, user, reason, commit):
+        LOG.info(
+            f"Moved alias {alias.id} ({alias.email}) of custom domain {alias.custom_domain_id} to custom domain trash"
+        )
+        return
+
     alias.delete_on = arrow.now().shift(days=+ALIAS_TRASH_DAYS)
     alias.delete_reason = reason
     alias.enabled = False
