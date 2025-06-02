f8ff735d810a925fc93f2156020b81f56b792078
Sahil Phule <sahilph@users.noreply.github.com>
2025-06-02 00:49:43 -0400

    Adapt tests for nginx smtp proxy

--- tests/test_SMTP_handler.py
+++ tests/test_SMTP_handler.py
@@ -4,86 +4,196 @@ import random
 import smtplib
 import ssl
 import pytest
+import SMTP_handler
 from email.message import EmailMessage
-
-import requests
-from aiosmtpd.controller import Controller
-
 from tests.utils import create_new_user
 from app.models import Alias, SMTPCredentials
 from app.db import Session
-
-import SMTP_handler
-
 from app.config import (
-    SMTP_HOST,
-    SMTP_PORT,
-    SMTP_INTERNAL_HOST,
+    EMAIL_DOMAIN,
+    SMTP_INTERNAL_HOST_IP,
     SMTP_INTERNAL_PORT,
-    EMAIL_DOMAIN
+    SMTP_INTERNAL_ACCESS_SECRET
 )
 
-SMTP_handler.main(SMTP_HOST, SMTP_PORT, daemon=True)
-
-@pytest.fixture
-def ssl_context():
-    return ssl._create_unverified_context()
-
-def test_implicit_ssl_connection(ssl_context):
-    with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT, context=ssl_context) as server:
-        code, message = server.noop()
-        assert code == 250, f"Expected 250, got {code}. Message: {message.decode()}"
-
-def test_auth_success(ssl_context):
+def test_auth_smtp_helper_success(flask_client):
     user = create_new_user()
     alias = Alias.create_new_random(user)
     user.enable_SMTP_aliases = True
-    VALID_USERNAME = alias.email
-    VALID_PASSWORD = SMTPCredentials.create(alias_id=alias.id)
+    valid_username = alias.email
+    valid_password = SMTPCredentials.create(alias_id=alias.id)
     alias.enable_SMTP = True
     Session.commit()
-    with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT, context=ssl_context) as server:
-        response = server.login(VALID_USERNAME, VALID_PASSWORD)
+
+    headers = {
+        "X-Secret": SMTP_INTERNAL_ACCESS_SECRET,
+        "Auth-Method": "login",
+        "Auth-User": valid_username,
+        "Auth-Pass": valid_password,
+        "Auth-Protocol": "smtp",
+    }
+    r= flask_client.get("/api/auth/smtp", headers=headers)
+
+    assert r.status_code == 200
+    assert r.headers["Auth-Status"] == "OK"
+    assert r.headers["Auth-Server"] == SMTP_INTERNAL_HOST_IP
+    assert int(r.headers["Auth-Port"]) == SMTP_INTERNAL_PORT
+
+def test_auth_smtp_helper_failure_no_secret(flask_client):
+    user = create_new_user()
+    alias = Alias.create_new_random(user)
+    user.enable_SMTP_aliases = True
+    valid_username = alias.email
+    valid_password = SMTPCredentials.create(alias_id=alias.id)
+    alias.enable_SMTP = True
+    Session.commit()
+
+    headers = {
+        "Auth-Method": "login",
+        "Auth-User": valid_username,
+        "Auth-Pass": valid_password,
+        "Auth-Protocol": "smtp",
+    }
+    r= flask_client.get("/api/auth/smtp", headers=headers)
+
+    assert r.status_code == 403
+    assert "Auth-Status" in r.headers and r.headers["Auth-Status"] != "OK"
+
+def test_auth_smtp_helper_failure_incomplete_headers(flask_client):
+    user = create_new_user()
+    alias = Alias.create_new_random(user)
+    user.enable_SMTP_aliases = True
+    valid_username = alias.email
+    valid_password = SMTPCredentials.create(alias_id=alias.id)
+    alias.enable_SMTP = True
+    Session.commit()
+
+    headers = {
+        "X-Secret": SMTP_INTERNAL_ACCESS_SECRET,
+        "Auth-User": valid_username,
+        "Auth-Pass": valid_password,
+    }
+    r= flask_client.get("/api/auth/smtp", headers=headers)
+
+    assert r.status_code == 400
+    assert "Auth-Status" in r.headers and r.headers["Auth-Status"] != "OK"
+
+def test_auth_smtp_helper_failure_wrong_user_pass(flask_client):
+    user = create_new_user()
+    alias = Alias.create_new_random(user)
+    user.enable_SMTP_aliases = True
+    SMTPCredentials.create(alias_id=alias.id)
+    invalid_username = "random-{}@{}".format(random.random(), EMAIL_DOMAIN)
+    invalid_password = "wrong_password"
+    alias.enable_SMTP = True
+    Session.commit()
+
+    headers = {
+        "X-Secret": SMTP_INTERNAL_ACCESS_SECRET,
+        "Auth-Method": "login",
+        "Auth-User": invalid_username,
+        "Auth-Pass": invalid_password,
+        "Auth-Protocol": "smtp",
+    }
+    r= flask_client.get("/api/auth/smtp", headers=headers)
+
+    assert r.status_code == 401
+    assert "Auth-Status" in r.headers and r.headers["Auth-Status"] != "OK"
+
+def test_auth_smtp_helper_failure_invalid_mechanism(flask_client):
+    user = create_new_user()
+    alias = Alias.create_new_random(user)
+    user.enable_SMTP_aliases = True
+    valid_username = alias.email
+    valid_password = SMTPCredentials.create(alias_id=alias.id)
+    alias.enable_SMTP = True
+    Session.commit()
+
+    headers = {
+        "X-Secret": SMTP_INTERNAL_ACCESS_SECRET,
+        "Auth-Method": "cram-md5",
+        "Auth-User": valid_username,
+        "Auth-Pass": valid_password,
+        "Auth-Protocol": "smtp",
+    }
+    r= flask_client.get("/api/auth/smtp", headers=headers)
+
+    assert r.status_code == 401
+    assert "Auth-Status" in r.headers and r.headers["Auth-Status"] != "OK"
+
+def test_auth_smtp_helper_failure_invalid_protocol(flask_client):
+    user = create_new_user()
+    alias = Alias.create_new_random(user)
+    user.enable_SMTP_aliases = True
+    valid_username = alias.email
+    valid_password = SMTPCredentials.create(alias_id=alias.id)
+    alias.enable_SMTP = True
+    Session.commit()
+
+    headers = {
+        "X-Secret": SMTP_INTERNAL_ACCESS_SECRET,
+        "Auth-Method": "login",
+        "Auth-User": valid_username,
+        "Auth-Pass": valid_password,
+        "Auth-Protocol": "pop3+ssl",
+    }
+    r= flask_client.get("/api/auth/smtp", headers=headers)
+
+    assert r.status_code == 401
+    assert "Auth-Status" in r.headers and r.headers["Auth-Status"] != "OK"
+
+SMTP_handler.main(SMTP_INTERNAL_HOST_IP, SMTP_INTERNAL_PORT, daemon=True)
+
+def test_SMTP_auth_success():
+    user = create_new_user()
+    alias = Alias.create_new_random(user)
+    user.enable_SMTP_aliases = True
+    valid_username = alias.email
+    valid_password = SMTPCredentials.create(alias_id=alias.id)
+    alias.enable_SMTP = True
+    Session.commit()
+    with smtplib.SMTP(SMTP_INTERNAL_HOST_IP, SMTP_INTERNAL_PORT) as server:
+        response = server.login(valid_username, valid_password)
         assert response[0] == 235, f"Expected 235 for successful login, got {response[0]}"
 
-def test_auth_failure(ssl_context):
+def test_SMTP_auth_failure():
     user = create_new_user()
     alias = Alias.create_new_random(user)
     user.enable_SMTP_aliases = True
     SMTPCredentials.create(alias_id=alias.id)
     alias.enable_SMTP = True
     Session.commit()
-    INVALID_USERNAME = "random-{}@{}".format(random.random(), EMAIL_DOMAIN)
-    INVALID_PASSWORD = "wrong_password"
-    with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT, context=ssl_context) as server:
+    invalid_username = "random-{}@{}".format(random.random(), EMAIL_DOMAIN)
+    invalid_password = "wrong_password"
+    with smtplib.SMTP(SMTP_INTERNAL_HOST_IP, SMTP_INTERNAL_PORT) as server:
         with pytest.raises(smtplib.SMTPAuthenticationError):
-            server.login(INVALID_USERNAME, INVALID_PASSWORD)
+            server.login(invalid_username, invalid_password)
 
 
-def test_send_email_with_valid_credentials(ssl_context):
+def test_SMTP_send_email_with_valid_credentials():
     user = create_new_user()
     alias = Alias.create_new_random(user)
     user.enable_SMTP_aliases = True
-    VALID_USERNAME = alias.email
-    VALID_PASSWORD = SMTPCredentials.create(alias_id=alias.id)
+    valid_username = alias.email
+    valid_password = SMTPCredentials.create(alias_id=alias.id)
     alias.enable_SMTP = True
     Session.commit()
     msg = EmailMessage()
     msg["Subject"] = "SMTP Test"
-    msg["From"] = VALID_USERNAME
+    msg["From"] = valid_username
     msg["To"] = "random-{}@nowhere.net".format(int(random.random()))
     msg.set_content("This is a test email.")
-    with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT, context=ssl_context) as server:
-        server.login(VALID_USERNAME, VALID_PASSWORD)
+    with smtplib.SMTP(SMTP_INTERNAL_HOST_IP, SMTP_INTERNAL_PORT) as server:
+        server.login(valid_username, valid_password)
         response = server.send_message(msg)
         assert response == {}, f"Expected empty dict, indicating no errors, got {response}"
 
-def test_from_spoofing(ssl_context):
+def test_SMTP_from_spoofing():
     user = create_new_user()
     alias = Alias.create_new_random(user)
     user.enable_SMTP_aliases = True
-    VALID_USERNAME = alias.email
-    VALID_PASSWORD = SMTPCredentials.create(alias_id=alias.id)
+    valid_username = alias.email
+    valid_password = SMTPCredentials.create(alias_id=alias.id)
     alias.enable_SMTP = True
     Session.commit()
     TO_EMAIL = "random-{}@nowhere.net".format(int(random.random()))
@@ -94,7 +204,7 @@ def test_from_spoofing(ssl_context):
     msg["To"] = TO_EMAIL
     msg.set_content("Testing From Spoofing.")
 
-    with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT, context=ssl_context) as server:
-        server.login(VALID_USERNAME, VALID_PASSWORD)
+    with smtplib.SMTP(SMTP_INTERNAL_HOST_IP, SMTP_INTERNAL_PORT) as server:
+        server.login(valid_username, valid_password)
         with pytest.raises(smtplib.SMTPResponseException):
             server.send_message(msg)
