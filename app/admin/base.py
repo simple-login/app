7673c7210c8c7bebc1b96a9c205acde0b7a73dad
Adrià Casajús <adria.casajus@proton.ch>
2026-03-09 18:28:12 +0100

    Admin time

--- app/admin/base.py
+++ app/admin/base.py
@@ -1,14 +1,31 @@
 from __future__ import annotations
 
 import sqlalchemy
-from flask import redirect, url_for, request, flash
+from flask import redirect, url_for, request, flash, session
 from flask_admin import expose, AdminIndexView
 from flask_admin.contrib import sqla
 from flask_login import current_user
 from markupsafe import Markup
+from time import time
 
 from app import models
+from app.dashboard.views.enter_admin import _ADMIN_GAP
 from app.models import AdminAuditLog, AuditLogActionEnum
+
+
+def _has_valid_admin_time() -> bool:
+    from app.config import ADMIN_FIDO_REQUIRED
+
+    if ADMIN_FIDO_REQUIRED == "none":
+        return True
+    admin_time = session.get("admin_time")
+    if not admin_time:
+        return False
+    if (time() - int(admin_time)) > _ADMIN_GAP:
+        return False
+    if ADMIN_FIDO_REQUIRED == "hardware" and not session.get("admin_hardware_auth"):
+        return False
+    return True
 
 
 def _admin_action_formatter(view, context, model, name):
@@ -35,12 +52,17 @@ class SLModelView(sqla.ModelView):
     edit_modal = True
 
     def is_accessible(self):
-        return current_user.is_authenticated and current_user.is_admin
+        return (
+            current_user.is_authenticated
+            and current_user.is_admin
+            and _has_valid_admin_time()
+        )
 
     def inaccessible_callback(self, name, **kwargs):
-        # redirect to login page if user doesn't have access
-        flash("You don't have access to the admin page", "error")
-        return redirect(url_for("dashboard.index", next=request.url))
+        if not current_user.is_authenticated or not current_user.is_admin:
+            flash("You don't have access to the admin page", "error")
+            return redirect(url_for("dashboard.index"))
+        return redirect(url_for("dashboard.enter_admin", next=request.url))
 
     def on_model_change(self, form, model, is_created):
         changes = {}
@@ -85,5 +107,6 @@ class SLAdminIndexView(AdminIndexView):
     def index(self):
         if not current_user.is_authenticated or not current_user.is_admin:
             return redirect(url_for("auth.login", next=request.url))
-
+        if not _has_valid_admin_time():
+            return redirect(url_for("dashboard.enter_admin", next=request.url))
         return redirect(url_for("admin.email_search.index"))
