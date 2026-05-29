4c39f7ce9502fb7eeb8a0a6138675fb58c1fdec0
Adrià Casajús <adria.casajus@proton.ch>
2026-05-29 14:42:28 +0200

    Fix racecond

--- tests/test_prarallel_limiter.py
+++ tests/test_prarallel_limiter.py
@@ -1,6 +1,5 @@
 import threading
 import time
-from typing import Optional
 
 import werkzeug.exceptions
 from flask_login import login_user
@@ -18,25 +17,32 @@ def test_parallel_limiter(flask_app):
             assert pl(lambda x: x)(loop_id) == loop_id
 
 
-def sleep_thread(pl: _InnerLock, sem: Optional[threading.Semaphore] = None):
-    if sem is not None:
-        sem.release()
-    pl(time.sleep)(1)
-
-
 def test_too_many_requests(flask_app):
     user = create_new_user()
+    sem = threading.Semaphore(0)
+    pl = _InnerLock("test", max_wait_secs=5)
+
+    def sleep_with_signal():
+        sem.release()
+        time.sleep(1)
+
+    def run_in_thread():
+        with flask_app.test_request_context():
+            login_user(user)
+            pl(sleep_with_signal)()
+
+    t = threading.Thread(target=run_in_thread)
+    t.daemon = True
+    t.start()
+    sem.acquire()
+
     with flask_app.test_request_context():
         login_user(user)
-        sem = threading.Semaphore(0)
-        pl = _InnerLock("test", max_wait_secs=5)
-        t = threading.Thread(target=sleep_thread, args=(pl, sem))
-        t.daemon = True
-        t.start()
-        sem.acquire()
+        got_exception = False
         try:
-            got_exception = False
-            pl(sleep_thread)(pl)
+            pl(lambda: None)()
         except werkzeug.exceptions.TooManyRequests:
             got_exception = True
-        assert got_exception
+
+    assert got_exception
+    t.join(timeout=2)
